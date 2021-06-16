95cf30f85580fcc8f564be88700ef17b6152c5eb
robedthomas <robedthomas@yahoo.com>
2021-06-16 09:18:16 -0500

    Add template selector to GridMap

---
+++ CivCulture/Utilities/TemplateSelectors.cs
@@ -0,0 +1,27 @@
+﻿using CivCulture_ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CivCulture.Utilities
+{
+    public class GridMapDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement element = container as FrameworkElement;
+            if (element != null && item != null)
+            {
+                if (item.GetType() == typeof(MapSpaceViewModel))
+                {
+                    return element.FindResource("mapSpaceViewModelTemplate") as DataTemplate;
+                }
+            }
+            return null;
+        }
+    }
+}
