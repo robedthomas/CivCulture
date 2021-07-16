ca29e2d7968c137d0296be8b13fc84f5e6c93c49
robedthomas <robedthomas@yahoo.com>
2021-07-15 22:52:37 -0500

    Add initial Terrain definitions

--- CivCulture_Model/Models/Terrain.cs
+++ CivCulture_Model/Models/Terrain.cs
@@ -8,6 +8,19 @@ namespace CivCulture_Model.Models
 {
     public class Terrain : GameComponent
     {
+        #region Static Members
+        public static Terrain Grassland;
+        public static Terrain Plains;
+        public static Terrain Mountains;
+
+        static Terrain()
+        {
+            Grassland = new Terrain("Grassland");
+            Plains = new Terrain("Plains");
+            Mountains = new Terrain("Mountains");
+        }
+        #endregion
+
         #region Fields
         #endregion
 
