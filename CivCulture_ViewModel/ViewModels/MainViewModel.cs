8bf82ba1d4b14f4894649d77c4af17b70b2e49ce
robedthomas <robedthomas@yahoo.com>
2021-12-10 14:18:54 -0600

    Add game config screen

--- CivCulture_ViewModel/ViewModels/MainViewModel.cs
+++ CivCulture_ViewModel/ViewModels/MainViewModel.cs
@@ -17,9 +17,11 @@ namespace CivCulture_ViewModel.ViewModels
     public class MainViewModel : BaseViewModel
     {
         #region Fields
+        private GameConfigViewModel configVM;
         private GameInstanceViewModel instanceVM;
         private RelayCommand newGameRC;
         private Visibility menuVisibility;
+        private Visibility configVisibility;
         private Visibility gameVisibility;
         #endregion
 
@@ -28,6 +30,19 @@ namespace CivCulture_ViewModel.ViewModels
 
         #region Properties
         public MainModel MainModel { get; set; }
+
+        public GameConfigViewModel GameConfigVM
+        {
+            get => configVM;
+            set
+            {
+                if (configVM != value)
+                {
+                    configVM = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         public GameInstanceViewModel GameInstanceVM
         {
@@ -68,6 +83,19 @@ namespace CivCulture_ViewModel.ViewModels
             }
         }
 
+        public Visibility ConfigVisibility
+        {
+            get => configVisibility;
+            set
+            {
+                if (configVisibility != value)
+                {
+                    configVisibility = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         public Visibility GameVisibility
         {
             get => gameVisibility;
@@ -86,18 +114,28 @@ namespace CivCulture_ViewModel.ViewModels
         public MainViewModel()
         {
             MenuVisibility = Visibility.Visible;
+            ConfigVisibility = Visibility.Collapsed;
             GameVisibility = Visibility.Collapsed;
             MainModel = new MainModel();
-            NewGameRC = new RelayCommand(GenerateNewGame);
+            GameConfigVM = new GameConfigViewModel(this);
+            NewGameRC = new RelayCommand((p) => ConfigureNewGame());
         }
         #endregion
 
         #region Methods
-        private void GenerateNewGame(object param)
+        private void ConfigureNewGame()
         {
-            MainModel.CurrentGame = MainModel.GenerateNewGame(new RandomMapGeneration(), new MapConfiguration() { Height = 2, Width = 3 }, new StandardTurnLogic());
+            MenuVisibility = Visibility.Collapsed;
+            ConfigVisibility = Visibility.Visible;
+            GameVisibility = Visibility.Collapsed;
+        }
+
+        public void GenerateNewGame(MapGeneration generation, MapConfiguration config, TurnLogic logic)
+        {
+            MainModel.CurrentGame = MainModel.GenerateNewGame(generation, config, logic);
             GameInstanceVM = new GameInstanceViewModel(MainModel.CurrentGame);
             MenuVisibility = Visibility.Collapsed;
+            ConfigVisibility = Visibility.Collapsed;
             GameVisibility = Visibility.Visible;
         }
         #endregion
