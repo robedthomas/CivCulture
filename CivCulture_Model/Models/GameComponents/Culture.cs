e08b3f7892dbcb8a2ddc322e352bc684416cee84
robedthomas <robedthomas@yahoo.com>
2022-01-18 12:10:58 -0600

    Add logic for determining available techs

--- CivCulture_Model/Models/GameComponents/Culture.cs
+++ CivCulture_Model/Models/GameComponents/Culture.cs
@@ -1,4 +1,5 @@
-﻿using CivCulture_Model.Models.Collections;
+﻿using CivCulture_Model.Events;
+using CivCulture_Model.Models.Collections;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,12 +9,14 @@ using System.Threading.Tasks;
 
 namespace CivCulture_Model.Models
 {
-    public class Culture : GameComponent
+    public class Culture : ResourceOwner
     {
         #region Events
+        public ValueChangedEventHandler<Technology> CurrentResearchChanged;
         #endregion
 
         #region Fields
+        private Technology currentResearch;
         #endregion
 
         #region Properties
@@ -30,6 +33,20 @@ namespace CivCulture_Model.Models
         public ObservableCollection<Technology> ResearchedTechnologies { get; protected set; }
 
         public ObservableCollection<Technology> AvailableTechnologies { get; protected set; }
+
+        public Technology CurrentResearch
+        {
+            get => currentResearch;
+            set
+            {
+                if (currentResearch != value)
+                {
+                    Technology oldValue = currentResearch;
+                    currentResearch = value;
+                    CurrentResearchChanged?.Invoke(this, new ValueChangedEventArgs<Technology>(oldValue, value));
+                }
+            }
+        }
 
         public TechModifierCollection TechModifiers { get; protected set; }
         #endregion
@@ -58,6 +75,8 @@ namespace CivCulture_Model.Models
                 foreach (Technology newTech in e.NewItems)
                 {
                     TechModifiers.AddRange(newTech.Template.Modifiers);
+                    AvailableTechnologies.Remove(newTech);
+                    MakeChildTechsAvailable(newTech);
                 }
             }
             if (e.OldItems != null)
@@ -65,6 +84,46 @@ namespace CivCulture_Model.Models
                 foreach (Technology oldTech in e.OldItems)
                 {
                     TechModifiers.RemoveRange(oldTech.Template.Modifiers);
+                    if (IsTechAvailable(oldTech.Template))
+                    {
+                        AvailableTechnologies.Add(oldTech);
+                    }
+                    MakeChildTechsUnavailable(oldTech);
+                }
+            }
+        }
+
+        private bool IsTechAvailable(TechnologyTemplate tech)
+        {
+            // If ALL of a tech's parent techs have been researched by this culture, add it to the available techs
+            if (tech.Parents.All(parentTech => ResearchedTechnologies.Any(researchedTech => researchedTech.Template == parentTech)))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void MakeChildTechsAvailable(Technology newTech)
+        {
+            foreach (TechnologyTemplate childTech in newTech.Template.Children)
+            {
+                // If ALL of a tech's parent techs have been researched by this culture, add it to the available techs
+                if (IsTechAvailable(childTech))
+                {
+                    AvailableTechnologies.Add(new Technology(childTech, this));
+                }
+            }
+        }
+
+        private void MakeChildTechsUnavailable(Technology oldTech)
+        {
+            // Remove ALL children of this tech from the available techs
+            foreach (TechnologyTemplate childTech in oldTech.Template.Children)
+            {
+                Technology targetTech = AvailableTechnologies.FirstOrDefault(tech => tech.Template == childTech);
+                if (targetTech != null)
+                {
+                    AvailableTechnologies.Remove(targetTech);
                 }
             }
         }
