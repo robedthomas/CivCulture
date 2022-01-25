2d47849cc7c88730f7681eb514d170559001baaa
robedthomas <robedthomas@yahoo.com>
2022-01-25 12:44:53 -0600

    Add simple early techs

--- CivCulture_Model/Models/GameInstance.cs
+++ CivCulture_Model/Models/GameInstance.cs
@@ -191,15 +191,36 @@ namespace CivCulture_Model.Models
         {
             ObservableCollection<TechnologyTemplate> techs = new ObservableCollection<TechnologyTemplate>();
 
-            TechnologyTemplate organizedLabor1 = new TechnologyTemplate("Organized Labor (I)", new ConsumeablesCollection() { { Fundamental.Progress, 5 } });
+            TechnologyTemplate permanentSettlement1 = new TechnologyTemplate("Permanent Settlement (I)", new ConsumeablesCollection() { { Fundamental.Progress, 5 } });
+            permanentSettlement1.Modifiers.Add(StatModification.CultureEnableBuilding, BuildingTemplate.MudHuts, null, null);
+            permanentSettlement1.Modifiers.Add(StatModification.SpaceJobs, JobTemplate.Builder, null, new TechModifier<decimal>(permanentSettlement1, 1));
+            techs.Add(permanentSettlement1);
+
+            //TechnologyTemplate permanentSettlement2 = new TechnologyTemplate("Permanent Settlement (II)", new ConsumeablesCollection() { { Fundamental.Progress, 5 } });
+            //permanentSettlement2.Modifiers.Add(StatModification.)
+
+            TechnologyTemplate agriculture1 = new TechnologyTemplate("Agriculture (I)", new ConsumeablesCollection() { { Fundamental.Progress, 10 } });
+            agriculture1.Modifiers.Add(StatModification.CultureEnableBuilding, BuildingTemplate.PrimitiveFarm, null, null);
+            agriculture1.Parents.Add(permanentSettlement1);
+            permanentSettlement1.Children.Add(agriculture1);
+            techs.Add(agriculture1);
+
+            TechnologyTemplate organizedLabor1 = new TechnologyTemplate("Organized Labor (I)", new ConsumeablesCollection() { { Fundamental.Progress, 10 } });
             organizedLabor1.Modifiers.Add(StatModification.JobOutputs, JobTemplate.Builder, Fundamental.Production, new TechModifier<decimal>(organizedLabor1, 0.5M));
+            organizedLabor1.Parents.Add(permanentSettlement1);
+            permanentSettlement1.Children.Add(organizedLabor1);
             techs.Add(organizedLabor1);
 
-            TechnologyTemplate badTech = new TechnologyTemplate("Bad Tech", new ConsumeablesCollection() { { Fundamental.Progress, 1 } });
-            badTech.Modifiers.Add(StatModification.PopNecessities, PopTemplate.HunterGatherer, Fundamental.Food, new TechModifier<decimal>(badTech, 0.5M));
-            badTech.Parents.Add(organizedLabor1);
-            organizedLabor1.Children.Add(badTech);
-            techs.Add(badTech);
+            TechnologyTemplate elderReverence1 = new TechnologyTemplate("Elder Reverence (I)", new ConsumeablesCollection() { { Fundamental.Progress, 5 } });
+            elderReverence1.Modifiers.Add(StatModification.SpaceJobs, JobTemplate.Elder, null, new TechModifier<decimal>(elderReverence1, 1));
+            techs.Add(elderReverence1);
+
+            TechnologyTemplate elderReverence2 = new TechnologyTemplate("Elder Reverence (II)", new ConsumeablesCollection() { { Fundamental.Progress, 10 } });
+            elderReverence2.Modifiers.Add(StatModification.JobInputs, JobTemplate.Elder, Fundamental.Food, new TechModifier<decimal>(elderReverence2, 0.5M));
+            elderReverence2.Modifiers.Add(StatModification.JobOutputs, JobTemplate.Elder, Fundamental.Progress, new TechModifier<decimal>(elderReverence2, 0.5M));
+            elderReverence2.Parents.Add(elderReverence1);
+            elderReverence1.Children.Add(elderReverence2);
+            techs.Add(elderReverence2);
 
             return techs;
         }
